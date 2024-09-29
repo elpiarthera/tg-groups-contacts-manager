9bc08560b11739f7a991a6f62b2aee7198d3757a
elpiarthera <artherasmg@gmail.com>
2024-09-29 21:36:14 +0200

    Update config and fix import issues

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const path = require('path');
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+}
 
-module.exports = {
-    webpack: (config) => {
-        config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-        return config;
-    },
-};
+module.exports = nextConfig
